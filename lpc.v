03c1715ea6564e7e01bdf88bd816db03234e54a2
Alexander Couzens <lynxis@fe80.eu>
2015-12-27 16:09:27 +0100

    rewritten lpc.v

--- lpc.v
+++ lpc.v
@@ -7,27 +7,26 @@
 	* out_cyctype_dir: type and direction. same as in LPC Spec 1.1
         * out_addr: 16-bit address
         * out_data: data read or written (1byte)
-	* out_latch: on rising edge all data must read.
+	* out_clock_enable: on rising edge all data must read.
  */
 
-module lpc(lpc_ad, lpc_clock, lpc_frame, lpc_reset, out_cyctype_dir, out_addr, out_data, out_latch);
-	input [3:0] lpc_ad;
-	input lpc_clock;
-	input lpc_frame;
-	input lpc_reset;
+module lpc(
+	input [3:0] lpc_ad,
+	input lpc_clock,
+	input lpc_frame,
+	input lpc_reset,
+	output [3:0] out_cyctype_dir,
+	output [31:0] out_addr,
+	output [7:0] out_data,
+	output out_clock_enable);
 
 	/* type and direction. same as in LPC Spec 1.1 */
-	output [3:0] out_cyctype_dir;
 
 	/* addr + data written or read */
-	output [31:0] out_addr;
-	output [7:0] out_data;
-
-	output reg out_latch;
 
 	/* state machine */
-	reg [2:0] state;
-	localparam reset = 1, start = 2, address = 3, tar = 4, sync = 5, io_data = 6;
+	reg [3:0] state = 0;
+	localparam idle = 0, start = 1, cycle_dir = 2, address = 3, tar = 4, sync = 5, read_data = 6, abort = 7;
 
 	/* counter used by some states */
 	reg [3:0] counter;
@@ -38,92 +37,134 @@ module lpc(lpc_ad, lpc_clock, lpc_frame, lpc_reset, out_cyctype_dir, out_addr, o
 	reg [31:0] addr;
 	reg [7:0] data;
 
-	always @(posedge lpc_clock or negedge lpc_reset)
+	always @(posedge lpc_clock or negedge lpc_reset) begin
+		if (~lpc_reset) begin
+			state <= idle;
+			counter <=  0;
+		end
+		else begin
+			if (counter != 0)
+				counter <= counter - 1;
+			else
+				case (state)
+					idle:
+						// wait for start condition
+						if (~lpc_frame && lpc_ad == 4'b1111)
+							state <= cycle_dir;
+					cycle_dir: begin
+						if (cyctype_dir[3:2] == 2'b00) begin /* i/o */
+							state <= address;
+							counter <= 4;
+						end
+						else if (cyctype_dir[3:2] == 2'b01) begin /* memory */
+							state <= address;
+							counter <= 8;
+						end
+						else begin /* dma or reserved not yet supported */
+							state <= idle;
+						end
+					end
+					address: begin
+						if (cyctype_dir[1]) begin /* write memory or i/o */
+							state <= read_data;
+							counter <= 2;
+						end
+						else begin /* read memory or i/o */
+							state <= tar;
+							counter <= 2;
+						end
+					end
+					tar: begin
+						state <= sync;
+					end
+					sync:
+						if (lpc_ad == 4'b0000)
+							if (cyctype_dir[3] == 0) begin /* i/o or memory */
+								state <= read_data;
+								counter <= 2;
+							end
+							else
+								state <= idle; /* dma or reserved */
+					read_data: begin
+						state <= idle;
+					end
+
+					abort: begin /* lpc abort */
+						counter <= 2;
+					end
+				endcase
+		end
+	end
+
+	always @(posedge lpc_clock)
 	begin
 		addr[31:16] <= 0; /* still unused - memory mode requires these bits */
 
-		if (~lpc_reset) begin
-			state <= reset;
-			addr <= 0;
-		end
-		else begin
-			case (state)
-				// wait for start segment
-				reset: begin
-					if (~lpc_frame && lpc_ad == 4'b0000) begin
-						out_latch <= 0;
-						state <= start;
-					end
-				end
+		case (state)
+			// wait for start segment
+			idle: begin
+			end
 
-				// read out mode (i/o memory dma)
-				start: begin
-					cyctype_dir <= lpc_ad;
-
-					if (lpc_ad[3:2] == 2'b00) begin
-						state <= address;
-						counter <= 0;
-					end
-					else begin
-						// unsupported mode, ignore
-						state <= reset;
-					end
+			cycle_dir: begin
+				cyctype_dir <= lpc_ad;
+			end
+			address: begin
+				case (cyctype_dir[3:2])
+				2'b00: begin /* 16 bit i/o */
+					addr[31:16] <= 0;
+					case (counter)
+						4:
+							addr[15:12] <= lpc_ad;
+						3:
+							addr[11:8] <= lpc_ad;
+						2:
+							addr[7:4] <= lpc_ad;
+						1:
+							addr[3:0] <= lpc_ad;
+					endcase
 				end
-
-				address: begin
-					// 16 bit address for io
-					if (counter >= 4) begin
-						counter <= 0;
-						state <= tar;
-					end
-					else begin
-						case (counter)
-							0:
-								addr[15:12] <= lpc_ad;
-							1:
-								addr[11:8] <= lpc_ad;
-							2:
-								addr[7:4] <= lpc_ad;
-							3:
-								addr[3:0] <= lpc_ad;
-						endcase
-						counter <= counter + 1;
-					end
+				2'b10: begin /* 32 bit memory */
+					addr[31:16] <= 0;
+					case (counter)
+						8:
+							addr[31:28] <= lpc_ad;
+						7:
+							addr[27:24] <= lpc_ad;
+						6:
+							addr[23:20] <= lpc_ad;
+						5:
+							addr[19:16] <= lpc_ad;
+						4:
+							addr[15:12] <= lpc_ad;
+						3:
+							addr[11:8] <= lpc_ad;
+						2:
+							addr[7:4] <= lpc_ad;
+						1:
+							addr[3:0] <= lpc_ad;
+					endcase
 				end
+				default: begin end
+				endcase
+			end
 
-				tar: begin
-					if (counter >= 2) begin
-						state <= sync;
-						counter <= 0;
+			read_data: begin
+				case (counter)
+					1:
+						data[7:4] <= lpc_ad[3:0];
+					0: begin
+						out_clock_enable <= 1;
+						data[3:0] <= lpc_ad[3:0];
 					end
-					else begin
-						counter <= counter + 1;
-					end
-				end
+				endcase
+			end
 
-				sync: begin
-					if (lpc_ad == 4'b0000) begin
-						state <= io_data;
-					end
-				end
+			idle: begin
+				out_clock_enable <= 0;
+			end
 
-				io_data: begin
-					if (counter >= 2) begin
-						state <= reset;
-						out_latch <= 1;
-					end
-					else begin
-						case (counter)
-							0:
-								data[7:4] <= lpc_ad[3:0];
-							1:
-								data[3:0] <= lpc_ad[3:0];
-						endcase
-						counter <= counter + 1;
-					end
-				end
-			endcase
-		end
+			default: begin end
+		endcase
 	end
 	assign out_cyctype_dir = cyctype_dir;
 	assign out_data = data;
