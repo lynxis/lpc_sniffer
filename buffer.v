d9f33688fdd376b94e93e957fca6621f530c79f2
Alexander Couzens <lynxis@fe80.eu>
2016-01-04 07:08:41 +0100

    buffer.v: implement read + write_clock_enable signals

--- buffer.v
+++ buffer.v
@@ -2,11 +2,12 @@
 
 module buffer #(parameter AW = 8, parameter DW = 8)
 	(
-		input write_clock,
+		input clock,
+		input write_clock_enable,
 		input [DW-1:0] write_data,
 		input [AW-1:0] write_addr,
 
-		input read_clock,
+		input read_clock_enable,
 		output reg [DW-1:0] read_data,
 		input [AW-1:0] read_addr);
 
@@ -14,9 +15,11 @@ module buffer #(parameter AW = 8, parameter DW = 8)
 
 	reg [DW-1: 0] ram [0: NPOS-1];
 
-	always @(posedge write_clock)
-		ram[write_addr] <= write_data;
+	always @(posedge clock)
+		if (write_clock_enable)
+			ram[write_addr] <= write_data;
 
-	always @(posedge read_clock)
-		read_data <= ram[read_addr];
+	always @(posedge clock)
+		if (read_clock_enable)
+			read_data <= ram[read_addr];
 endmodule
