8385b4ff3e3e97794abb76b63e2b1d357f9565ff
Joerg Albert <jal2@gmx.de>
2017-07-14 09:43:23 +0200

    put tasks for lpc testbenches into lpc_lib.v
    added *.vpp to .gitignore

---
+++ lpc_lib.v
@@ -0,0 +1,119 @@
+// some common LPC bus tasks
+// you must have these global variables defined in the including module: lpc_ad, lpc_clock
+
+// put a 16 bit address on the bus
+// assume that lpc_clock is 0 when called
+// lpc_clock is 0 afterwards
+task lpc_addr16;
+   input integer addr;
+   
+   begin
+      // four cycles for the address - highest nibble first!
+      lpc_ad = (addr >> 12) & 'hf;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+      lpc_ad = (addr >> 8) & 'hf;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+      lpc_ad = (addr >> 4) & 'hf;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+      lpc_ad = addr & 'hf;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+      
+   end
+endtask // lpc_addr16
+
+//put a 32 bit address on the bus
+// assume that lpc_clock is 0 when called
+// lpc_clock is 0 afterwards
+task lpc_addr32;
+   input integer addr;
+   begin
+      lpc_addr16((addr >> 16) & 'hffff);
+      lpc_addr16(addr & 'hffff);
+   end
+endtask
+
+//put a 8 bit data on the bus
+// assume that lpc_clock is 0 when called
+// lpc_clock is 0 afterwards
+task lpc_data;
+   input integer data;
+   begin
+      // two cycles for data - lower nibble first!
+
+      lpc_ad = data & 'hf;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+      lpc_ad = (data >> 4) & 'hf;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+   end
+endtask // load_data
+
+// send a lpc start frame
+// assume that lpc_clock is 0 when called
+// lpc_clock is 0 afterwards
+
+task lpc_start;
+   input integer nr_frame_clks; // how many clocks shall frame be asserted?
+   input integer start_value; // the value of lpc_ad when frame is asserted in all but the last cycles
+   input integer start_value_last; // the value of lpc_ad when frame is asserted in the last cycle
+   
+   begin
+      
+      lpc_frame = 0;
+      if (nr_frame_clks > 1) begin
+	 lpc_ad = start_value;
+	 repeat(nr_frame_clks - 1) begin
+	    #1 lpc_clock = 1;
+	    #1 lpc_clock = 0;
+	 end
+      end
+      lpc_ad = start_value_last;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+      lpc_frame = 1;
+   end
+endtask // lpc_start
+
+//put a nowait sync (ad == 0) on the bus
+// assume that lpc_clock is 0 when called
+// lpc_clock is 0 afterwards
+task lpc_sync;
+   begin
+      lpc_ad = 0;
+      #1 lpc_clock = 1;
+      #1 lpc_clock = 0;
+   end
+endtask
+
+//put nr_sync many short sync (lpc_ad == 5) on the bus
+// assume that lpc_clock is 0 when called
+// lpc_clock is 0 afterwards
+task lpc_shortsync;
+   input integer nr_sync;
+   begin
+      lpc_ad = 5;
+      repeat(nr_sync) begin
+	 #1 lpc_clock = 1;
+	 #1 lpc_clock = 0;
+      end
+   end
+endtask   
+
+//put nr_sync many long sync (lpc_ad == 6) on the bus
+// assume that lpc_clock is 0 when called
+// lpc_clock is 0 afterwards
+task lpc_longsync;
+   input integer nr_sync;
+   begin
+      lpc_ad = 6;
+      repeat(nr_sync) begin
+	 #1 lpc_clock = 1;
+	 #1 lpc_clock = 0;
+      end
+   end
+endtask   
--- lpc_tb.v
+++ lpc_tb.v
@@ -29,85 +29,8 @@ module lpc_tb ();
 	    .out_clock_enable(out_clock)
 	    );
 
-   // put a 16 bit address on the bus
-   // assume that lpc_clock is 0 when called
-   // lpc_clock is 0 afterwards
-   task load_addr16;
-      input integer addr;
-      
-     begin
-	// four cycles for the address - highest nibble first!
-	lpc_ad = (addr >> 12) & 'hf;
-	#1 lpc_clock = 1;
-	#1 lpc_clock = 0;
-	lpc_ad = (addr >> 8) & 'hf;
-	#1 lpc_clock = 1;
-	#1 lpc_clock = 0;
-	lpc_ad = (addr >> 4) & 'hf;
-	#1 lpc_clock = 1;
-	#1 lpc_clock = 0;
-	lpc_ad = addr & 'hf;
-	#1 lpc_clock = 1;
-	#1 lpc_clock = 0;
-	
-     end
-   endtask // load_addr16
-
-   //put a 32 bit address on the bus
-   // assume that lpc_clock is 0 when called
-   // lpc_clock is 0 afterwards
-   task load_addr32;
-      input integer addr;
-      begin
-	 load_addr16((addr >> 16) & 'hffff);
-	 load_addr16(addr & 'hffff);
-      end
-   endtask //load_addr32
+`include "lpc_lib.v"
    
-   //put a 8 bit data on the bus
-   // assume that lpc_clock is 0 when called
-   // lpc_clock is 0 afterwards
-   task load_data;
-      input integer data;
-      begin
-	 // two cycles for data - lower nibble first!
-
-	 lpc_ad = data & 'hf;
-	 #1 lpc_clock = 1;
-	 #1 lpc_clock = 0;
-	 lpc_ad = (data >> 4) & 'hf;
-	 #1 lpc_clock = 1;
-	 #1 lpc_clock = 0;
-      end
-   endtask // load_data
-
-   // send a lpc start frame
-   // assume that lpc_clock is 0 when called
-   // lpc_clock is 0 afterwards
-
-   task lpc_start;
-      input integer nr_frame_clks; // how many clocks shall frame be asserted?
-      input integer start_value; // the value of lpc_ad when frame is asserted in all but the last cycles
-      input integer start_value_last; // the value of lpc_ad when frame is asserted in the last cycle
-      
-      begin
-	 
-	 lpc_frame = 0;
-	 if (nr_frame_clks > 1) begin
-	    lpc_ad = start_value;
-	    repeat(nr_frame_clks - 1) begin
-	       #1 lpc_clock = 1;
-	       #1 lpc_clock = 0;
-	    end
-	 end
-	 lpc_ad = start_value_last;
-	 #1 lpc_clock = 1;
-	 #1 lpc_clock = 0;
-	 lpc_frame = 1;
-      end
-   endtask // lpc_start
-   
-
    initial begin
       $dumpfile ("lpc_tb.vcd");
       $dumpvars (0, lpc_tb);
@@ -125,19 +48,13 @@ module lpc_tb ();
       
       // LPC start: frame asserted for one cycle with ad == 0000
       lpc_start(1, 0, 0);
- /*     
-      lpc_frame = 0;
-      lpc_ad = 0;
-      #1 lpc_clock = 1;
-      #1 lpc_clock = 0;
-      lpc_frame = 1;
-*/      
+      
       // start with i/o read access
       lpc_ad = 4'b0000; // bit 3:2 = 0 --> i/o, bit 1 = 0 --> read
       #1 lpc_clock = 1;
       #1 lpc_clock = 0;
       
-      load_addr16(test_addr);
+      lpc_addr16(test_addr);
       
       // tar
       lpc_ad = 4'bzzzz;
@@ -151,7 +68,7 @@ module lpc_tb ();
       #1 lpc_clock = 1;
       #1 lpc_clock = 0;
 
-      load_data(test_data);
+      lpc_data(test_data);
       
       
      // tar
