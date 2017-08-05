5abb5e2c654af178cd1ca3554d2deec0780129c2
Alexander Couzens <lynxis@fe80.eu>
2017-08-05 20:20:08 +0200

    add helloonechar to print a char every 1 sek

--- helloonechar.v
+++ helloonechar.v
@@ -1,8 +1,9 @@
-module helloonechar #(parameter CLOCK_FREQ = 12000000, parameter BAUD_RATE = 115200)
+module helloonechar #(parameter CLOCK_FREQ = 12000000, parameter BAUD_RATE = 9600)
 (
 	input ext_clock,
 	output uart_tx_pin,
 	output uart_tx_led,
+	output counter_led,
 	output uart_clock_led);
 
 	wire reset;
@@ -12,20 +13,33 @@ module helloonechar #(parameter CLOCK_FREQ = 12000000, parameter BAUD_RATE = 115
 	wire [7:0] uart_data;
 	wire uart_clock_enable;
 	wire uart_clock;
+	reg [32:0] count;
 
 	power_on_reset POR(
 		.clock(ext_clock),
 		.reset(reset));
 
 	always @(*) begin
-		uart_data = 8'h65;
+		uart_data = 8'h1f;
 	end
 
-	always @(negedge uart_ready or negedge reset) begin
+	always @(negedge ext_clock or negedge reset) begin
 		if (~reset) begin
 			uart_clock_enable <= 1;
+			count <= 12000000;
 		end else begin
-			uart_clock_enable <= 0;
+			if (~uart_ready)
+				uart_clock_enable <= 0;
+			else  begin
+				if (count == 0) begin
+					counter_led <= ~counter_led;
+					count <= 12000000;
+					uart_clock_enable <= 1;
+				end
+				else begin
+					count <= count - 1;
+				end
+			end
 		end
 	end
 
