e167c90613b440f8b90bc5103aa9b2207238dd08
Alexander Couzens <lynxis@fe80.eu>
2017-08-06 02:05:22 +0200

    ringbuffer_tb: use always clock and replace timings

--- ringbuffer_tb.v
+++ ringbuffer_tb.v
@@ -8,7 +8,7 @@ module ringbuffer_tb ();
 	reg clock;
 	reg reset;
 
-	wire [1:0] write_data;
+	reg [1:0] write_data;
 	wire [1:0] read_data;
 	wire empty;
 	wire overflow;
@@ -24,25 +24,22 @@ module ringbuffer_tb ();
 			.empty(empty),
 			.overflow(overflow));
 
+	always
+		#1 clock = ~clock;
+
 	initial begin
 		$dumpfile ("ringbuffer_tb.vcd");
-		$dumpvars (1, ringbuffer_tb);
-		#1;
+		$dumpvars (0, ringbuffer_tb);
 		clock = 0;
 		write_clock_enable = 0;
 		read_clock_enable = 0;
 		reset = 0;
-		#1;
-		clock = 1;
-		#1;
+		write_data = 2'b11;
+		#6;
 		reset = 1;
-		clock = 0;
+		#6
 		if (read_data != 0) begin
 			$display("After reset read_data != 0");
-			$stop;
-		end
-		if (write_data != 0) begin
-			$display("After reset write_data != 0");
 			$stop;
 		end
 		if (~empty) begin
@@ -56,13 +53,8 @@ module ringbuffer_tb ();
 
 		// try to read even if the buffer is empty
 		#1;
-		clock = 1;
 		read_clock_enable = 1;
-		#1;
-		clock = 0;
-		read_clock_enable = 1;
-		#1;
-		clock = 1;
+		#2;
 		read_clock_enable = 0;
 		#1;
 		if (read_data != 0) begin
@@ -73,72 +65,64 @@ module ringbuffer_tb ();
 		// check write, read, write, write, write,read, read, write, read, read
 		#1;
 		write_clock_enable = 1;
-		#1;
+		#2;
 		write_clock_enable = 0;
 		#1;
-		if (write_data != 1) begin
-			$display("write_data doesn\'t increase when writing to an empty ringbuffer");
-			$stop;
-		end
 		if (empty || overflow) begin
 			$display("buffer should not empty nor overflowed");
 			$stop;
 		end
-		#1;
+		#2;
 		read_clock_enable = 1;
-		#1;
+		#2;
 		if (~empty) begin
 			$display("buffer is empty, but not signaling emptyness");
 			$stop;
 		end
-		if (read_data != 1) begin
+		if (read_data != 2'b11) begin
 			$display("read_data doesn\'t increase after read");
 			$stop;
 		end
-		#1;
+		#2;
 		read_clock_enable = 0;
-		#1;
+		#2;
 		// ringbuffer is now empty again, read_data & write_data should show = 1
-		#1;
+		#2;
 		write_clock_enable = 1;
-		#1;
+		#2;
 		write_clock_enable = 0;
-		#1;
+		#2;
 		write_clock_enable = 1;
-		#1;
+		#2;
 		write_clock_enable = 0;
-		#1;
+		#2;
 		// buffer should be full
 		write_clock_enable = 1;
-		#1;
+		#2;
 		write_clock_enable = 0;
 		if (~overflow) begin
 			$display("overflow not signaled even it\'s full");
 			$stop;
 		end
-		if (write_data != 0) begin
-			$display("write_data not correct. Expected %x, read %x", 'h0, write_data);
-			$stop;
-		end
-		#1;
+		#2;
 		read_clock_enable = 1;
-		#1;
+		#2;
 		read_clock_enable = 0;
 		if (overflow) begin
 			$display("overflow should *NOT* signaled anymore");
 			$stop;
 		end
-		#1;
+		#2;
 		read_clock_enable = 1;
-		#1;
+		#2;
 		read_clock_enable = 0;
-		#1;
+		#2;
 		read_clock_enable = 1;
-		#1;
+		#2;
 		read_clock_enable = 0;
-		#1;
+		#2;
 		read_clock_enable = 1;
-		#1;
+		#2;
 		read_clock_enable = 0;
 		if (~empty) begin
 			$display("buffer should empty, but it doesn\'t signaling this");
